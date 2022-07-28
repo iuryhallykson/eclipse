d2a02177ad3758d82828afb84a305a2ae50e864b
Iury Hallykson <110191542+iuryhallykson@users.noreply.github.com>
2022-07-28 14:48:25 -0300

    Adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/iuryhallykson/git/HelloGit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.iuryhallykson.git;
+
+/**
+ * @author iuryh
+ * Exemplo de integração do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
