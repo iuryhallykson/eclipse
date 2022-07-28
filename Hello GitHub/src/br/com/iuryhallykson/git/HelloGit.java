d95167eed927616c639ae91c784d80b283050447
Iury Hallykson <110191542+iuryhallykson@users.noreply.github.com>
2022-07-28 15:58:04 -0300

    Entrada de dados em Java

--- Hello GitHub/src/br/com/iuryhallykson/git/HelloGit.java
+++ Hello GitHub/src/br/com/iuryhallykson/git/HelloGit.java
@@ -7,12 +7,31 @@ package br.com.iuryhallykson.git;
  * @author iuryh
  * Exemplo de integração do Eclipse com o GitHub
  */
+import java.util.Locale;
+import java.util.Scanner;
+
 public class HelloGit {
 
 	
-	public static void main(String[] args) {
-		System.out.println("Hello Git");
-		System.out.println("Iury Hallykson");
+	public static void main (String[] args) {
+		
+		Locale.setDefault(Locale.US);
+	    Scanner sc = new Scanner(System.in);
+	    
+	   String x;
+	   int y;
+	   double z;
+	   x = sc.next();
+	   y = sc.nextInt();
+	   z = sc.nextDouble();
+	   
+	   System.out.println("Dados digitados: ");
+	   System.out.println(x);
+	   System.out.println(y);
+	   System.out.println(z);
+	    
+	    sc.close();
+	 
 
 	}
 
